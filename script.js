65c4eaacdcd245687875abe3dfe15bd4088f586b
Joel Mbuyi <165423496+JoelMbuyi05@users.noreply.github.com>
2025-05-09 19:48:13 +0200

    made some important changes

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-window.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-      document.body.style.animationPlayState = "running"; 
-    }, 500); 
+document.addEventListener('DOMContentLoaded', function() {
+  const observerOptions = {
+      threshold: 0.1,
+      rootMargin: '0px'
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('show');
+          }
+      });
+  }, observerOptions);
+
+  // Observe all animate elements
+  document.querySelectorAll('.animate-down, .animate-up').forEach((el) => {
+      observer.observe(el);
   });
-  
-window.addEventListener("beforeunload", () => {
-    document.body.classList.add("fade-out");
+
+  const hamburger = document.querySelector(".hamburger");
+  const navbarLinks = document.querySelector(".navbar-links");
+  const navItems = document.querySelectorAll(".navbar-links li a");
+
+  hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navbarLinks.classList.toggle("active");
+  })
+
+  navItems.forEach(link => {
+    link.addEventListener("click", () =>{
+      hamburger.classList.remove("active");
+      navbarLinks.classList.remove("active");
+    });
   });
-  
+});
